b6667d47a2124704913861033e0077f321f24cc9
Javier Urbina <urbinarathgeb@gmail.com>
2023-09-27 23:44:47 -0300

    rustic implementation of multilanguage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), astroI18next()],
   image: {
     domains: ["cloudinary.com"],
     remotePatterns: [{ protocol: "https" }],
